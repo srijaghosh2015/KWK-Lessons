22113d33fd03518a81b7bcfca7f22699e96914b6
Srija Ghosh <63970343+srijaghosh2015@users.noreply.github.com>
2021-07-28 14:58:25 -0400

    Add files via upload

---
+++ MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Srija Ghosh on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var label3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func displayFactsButton(_ sender: Any) {
+        
+    }
+    
+}
+
