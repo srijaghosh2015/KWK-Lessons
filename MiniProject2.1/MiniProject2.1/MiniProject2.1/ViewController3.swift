ddeaeb2503bb7f3bfedd867b8786131f1ae42faf
Srija Ghosh <63970343+srijaghosh2015@users.noreply.github.com>
2021-07-30 10:36:38 -0400

    Add files via upload
    Mini Project styling

---
+++ MiniProject2.1/MiniProject2.1/MiniProject2.1/ViewController3.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController3.swift
+//  ViewController3
+//
+//  Created by Srija Ghosh on 7/29/21.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    @IBOutlet weak var label3: UILabel!
+    
+    @IBOutlet weak var textField3: UITextField!
+    
+    @IBOutlet weak var response3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        if let response = textField3.text{
+            if response.lowercased() == "42"{
+                response3.text = "Wowww, you are the ruler of the universe! Tell me your secrets"
+            }
+            else{
+                response3.text = "Don't worry, very few people know the answer"
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MiniProject2.1/MiniProject2.1/MiniProject2.1Tests/MiniProject2_1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MiniProject2_1Tests.swift
+//  MiniProject2.1Tests
+//
+//  Created by Srija Ghosh on 7/29/21.
+//
+
+import XCTest
+@testable import MiniProject2_1
+
+class MiniProject2_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
