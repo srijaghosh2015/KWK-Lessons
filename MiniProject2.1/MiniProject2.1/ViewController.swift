a87c33864bc350482440a7762470df27e6e59a3f
Srija Ghosh <63970343+srijaghosh2015@users.noreply.github.com>
2021-07-30 00:22:04 -0400

    Add files via upload

---
+++ MiniProject2.1/MiniProject2.1/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MiniProject2.1
+//
+//  Created by Srija Ghosh on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var response1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submit1(_ sender: Any) {
+        if let response = textField1.text{
+            if response.lowercased() == "john adams" || response.lowercased() == "adams"{
+                response1.text = "good job!"
+            }
+            else{
+                response1.text = "boo!"
+            }
+        }
+    }
+    
+}
+
---
+++ MiniProject2.1/MiniProject2.1/ViewController2.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController2.swift
+//  ViewController2
+//
+//  Created by Srija Ghosh on 7/29/21.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
+    @IBOutlet weak var response2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        if let response = textField2.text{
+            if response.lowercased() == "2014"{
+                response2.text = "yayy!"
+            }
+            else{
+                response2.text = "boo!"
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
